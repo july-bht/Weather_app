7be2b37e9bbd30bc035ef2e82e3436f203917b59
OliverC-123 <71122249+OliverC-123@users.noreply.github.com>
2023-01-17 12:23:23 +0100

    removed from hourly

--- ionicApp/src/components/Hourly.js
+++ ionicApp/src/components/Hourly.js
@@ -1,10 +1,6 @@
-import Fetch from "./Fetch";
+
 import HourWave from "../svg/hourWave";
 const Hourly = () => {
-  const getData = Fetch();
-  function currentDate(time) {
-    return new Date(time * 1000).toLocaleDateString([], "da-DK");
-  }
   return (
     <>
       <h4 className="hourly__title">Hourly</h4>
